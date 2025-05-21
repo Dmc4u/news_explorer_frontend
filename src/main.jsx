989cf6f31a407279c9b8163b4828d8adc66793b6
Dmc4u <dmsesbiz2005@gmail.com>
2025-05-21 19:22:29 +0300

    fix: implement reviewer recommendations

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <BrowserRouter basename="/news_explorer_frontend/"> */}
-    <BrowserRouter>
+   <BrowserRouter basename="/news_explorer_frontend/"> 
       <App />
     </BrowserRouter>
   </React.StrictMode>
